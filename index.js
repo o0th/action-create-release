787978e2cfca8f2765d8f7e8989b1495e84814e3
o0th <o0th@pm.me>
2024-10-01 17:18:06 +0200

    feat: createRef

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const fs = require('node:fs');
-const path = require('node:path');
-const core = require('@actions/core');
-const github = require('@actions/github');
+import fs from 'node:fs'
+import path from 'node:path'
+import core from '@actions/core'
+import github from '@actions/github'
 
 const regexes = {
   'package.json': /"version": "(?<version>\d.\d.\d)"/,
   'build.zig.zon': /.version = "(?<version>\d.\d.\d)"/
 }
 
-const files = fs.readdirSync(path.join(__dirname));
+const files = fs.readdirSync(path.join('.'));
 const file = files.find((file) => regexes.hasOwnProperty(file))
 
 if (!file) {
@@ -16,19 +16,19 @@ if (!file) {
   process.exit(1)
 }
 
-const content = fs.readFileSync(path.join(__dirname, file), 'utf8')
+const content = fs.readFileSync(path.join('.', file), 'utf8')
 const matches = content.match(regexes[file])
 
 if (!matches.groups.version) {
   core.error(`Couldn't find version in ${file}`)
 }
 
+const version = matches.groups.version
 const token = core.getInput('token');
 const [owner, repo] = core.getInput('repository').split('/');
 const sha = core.getInput('sha');
 
-const result = github.rest.git.getRef({
-  owner, repo, ref: `v${version}`
+const octokit = github.getOctokit(token)
+const response = await octokit.rest.git.createRef({
+  owner, repo, ref: `ref/tags/v${version}`
 })
-
-core.notice(result);
